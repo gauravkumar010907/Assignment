c1df294ec97be017aea2b8f78993c0600b5998ae
Gaurav Kumar <gauravkumar010907@gmail.com>
2022-09-16 01:21:15 +0530

    assignment

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1_Csharp;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Litware");
+
+            Console.WriteLine("Enter Employee No.");
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Salary");
+            double sal = Convert.ToDouble(Console.ReadLine());
+
+            Employee emp = new Employee(no, name, sal);
+            emp.CalculatedSalary();
+        }
+    }
+}
