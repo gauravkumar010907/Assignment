219486006646067e6b61259d0c229e00ad5de839
Gaurav Kumar <gauravkumar010907@gmail.com>
2022-09-24 20:13:07 +0530

    Hotel Management

---
+++ Hotel Management System/Migrations/BillDb/20220924142558_abc.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_Management_System.Migrations.BillDb
+{
+    public partial class abc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
